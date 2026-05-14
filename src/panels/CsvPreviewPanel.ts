135ba447c66c2102d47ed11bd25590f02325dff7
Giacomo Accursi <giacomo.accursi@gmail.com>
2026-05-14 11:25:41 +0200

    fix: toolbar icons rendering and descriptive tooltips

--- src/panels/CsvPreviewPanel.ts
+++ src/panels/CsvPreviewPanel.ts
@@ -259,21 +259,19 @@ export class CsvPreviewPanel {
   <div id="app">
     <div class="toolbar">
       <div class="toolbar-left">
-        <button id="refreshBtn" class="btn" title="Refresh">
+        <button id="refreshBtn" class="btn" title="Reload file from disk">
           <svg width="16" height="16" viewBox="0 0 16 16">
-            <path fill="currentColor" d="M13.5 2.5a1 1 0 0 1 1 1v3.5a1 1 0 0 1-1 1h-3.5a.5.5 0 1 1 0-1H13V3.5a.5.5 0 0 1 .5-.5z"/>
-            <path fill="currentColor" d="M13.354 3.354a.5.5 0 0 1 0 .707A7 7 0 1 1 3 8a.5.5 0 0 1 1 0 6 6 0 1 0 9.061-5.146.5.5 0 0 1 .293-.9z"/>
+            <path fill="currentColor" d="M13 2v4H9l1.6-1.6A4.5 4.5 0 0 0 8 3.5a4.5 4.5 0 1 0 4.5 4.5h1A5.5 5.5 0 1 1 8 2.5c1.4 0 2.7.5 3.6 1.4L13 2z"/>
           </svg>
         </button>
-        <button id="openAsTextBtn" class="btn" title="Open as Text">
+        <button id="openAsTextBtn" class="btn" title="Open file as plain text">
           <svg width="16" height="16" viewBox="0 0 16 16">
-            <path fill="currentColor" d="M5 2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H5zm0 1h6v10H5V3z"/>
-            <path fill="currentColor" d="M6 5h4v1H6V5zm0 2h4v1H6V7zm0 2h3v1H6V9z"/>
+            <path fill="currentColor" d="M3 1h7l3 3v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zm7 0v3h3L10 1zM5 6h6v1H5V6zm0 2h6v1H5V8zm0 2h4v1H5v-1z"/>
           </svg>
         </button>
-        <button id="colorBtn" class="btn" title="Toggle column colors">
+        <button id="colorBtn" class="btn" title="Toggle column colors for better readability">
           <svg width="16" height="16" viewBox="0 0 16 16">
-            <path fill="currentColor" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zm0 1.5a5.5 5.5 0 0 1 4.9 3H8V2.5zm-1 0v3H3.1A5.5 5.5 0 0 1 7 2.5zM2.5 8A5.5 5.5 0 0 1 3.1 6.5H7V9.5H3.1A5.5 5.5 0 0 1 2.5 8zm4.5 3.5v2a5.5 5.5 0 0 1-3.9-3h3.9zm1 2v-2h3.9a5.5 5.5 0 0 1-3.9 2zM8 9.5V6.5h4.9a5.5 5.5 0 0 1 0 3H8z"/>
+            <path fill="currentColor" d="M8 1C4.1 1 1 4.1 1 8s3.1 7 7 7 7-3.1 7-7-3.1-7-7-7zm0 1c1.2 0 2.3.4 3.2 1H8V3h-.5V2.02c.2-.01.3-.02.5-.02zM4.8 3h2.7v3H2.3A5.97 5.97 0 0 1 4.8 3zM2 8c0-.4 0-.7.1-1h5.4v2H2.1C2 8.7 2 8.4 2 8zm2.8 5A5.97 5.97 0 0 1 2.3 10h5.2v3H4.8zm3.7 0V10h5.2a5.97 5.97 0 0 1-2.5 3H8.5zm5.4-4H8.5V6h5.4c.1.3.1.6.1 1s0 .7-.1 1z"/>
           </svg>
         </button>
         <input type="text" id="searchInput" class="search-input" placeholder="Search..." />
